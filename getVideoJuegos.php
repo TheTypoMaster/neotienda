497bc427ef84c609fb3fef4b325ba85132073738
Luis Arismendi <luisfelipe.arismendi@gmail.com>
2015-03-23 21:35:48 -0430

    act

--- getVideoJuegos.php
+++ getVideoJuegos.php
@@ -9,26 +9,22 @@ $tipo=$_REQUEST["tipo"];
 $titulo=$_REQUEST["titulo"];
 
 $query=mysql_query("SELECT 
-						ps_product_lang.id_product id, 
-						ps_product_lang.name, 
-						ps_product_shop.price
-					FROM 
-						ps_product_lang, 
-						ps_lang, 
-						ps_category_product, 
-						ps_category_lang,
-						ps_product_shop 
-					WHERE 
-						ps_product_lang.id_lang = ps_lang.id_lang 
-						AND ps_product_lang.id_product = ps_category_product.id_product 
-						AND ps_category_lang.id_category = ps_category_lang.id_category 
-						AND ps_product_shop.id_product = ps_category_product.id_product 
-						AND ps_product_lang.id_lang = 2 
-						AND ps_product_lang.name LIKE '%".$titulo."%' 
-						AND ps_category_lang.name LIKE '%".$plataforma."%' 
-						AND ps_category_lang.id_lang = 2 
-					GROUP BY id
-					ORDER BY ps_product_lang.name");
+                        ps_product_lang.id_product id,
+                        ps_product_lang.name,
+                        ps_product_shop.price
+                    FROM
+                        ps_category_lang,
+                        ps_category_product,
+                        ps_product_shop,
+                        ps_product_lang
+                    WHERE
+                        ps_category_lang.name LIKE '%".$plataforma."%'
+                        AND ps_category_lang.id_category = ps_category_product.id_category
+                        AND ps_category_product.id_category = ps_product_shop.id_category_default
+                        AND ps_product_shop.id_product = ps_product_lang.id_product
+                        AND ps_product_lang.name LIKE '%".$titulo."%'
+                    GROUP BY id
+                    ORDER BY ps_product_lang.name");
 $json=array();
  
 while($student=mysql_fetch_array($query)){
